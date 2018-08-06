b3b4be1017a59d600b27a9700ca752dfdfc0986c
rajni <mehrar4600@gmail.com>
2018-08-06 11:13:47 -0400

    base backend setup

---
+++ backend/migrations/20180806140704-create-job-model.js
@@ -0,0 +1,45 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Job_Models', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      job_title: {
+        type: Sequelize.STRING
+      },
+      job_description: {
+        type: Sequelize.STRING
+      },
+      job_type: {
+        type: Sequelize.STRING
+      },
+      company_name: {
+        type: Sequelize.STRING
+      },
+      company_site: {
+        type: Sequelize.STRING
+      },
+      company_address: {
+        type: Sequelize.STRING
+      },
+      time: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Job_Models');
+  }
+};
---
+++ backend/models/job_model.js
@@ -0,0 +1,16 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var Job_Model = sequelize.define('Job_Model', {
+    job_title: DataTypes.STRING,
+    job_description: DataTypes.STRING,
+    job_type: DataTypes.STRING,
+    company_name: DataTypes.STRING,
+    company_site: DataTypes.STRING,
+    company_address: DataTypes.STRING,
+    time: DataTypes.STRING
+  }, {});
+  Job_Model.associate = function(models) {
+    // associations can be defined here
+  };
+  return Job_Model;
+};
